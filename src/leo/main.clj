d27ad7908d37e0ce848b5569ef664e91b8df58b9
Paulo Feodrippe <pfeodrippe@gmail.com>
2024-10-22 08:01:43 -0400

    Revert leo.main

--- src/leo/main.clj
+++ src/leo/main.clj
@@ -1,9 +1,7 @@
 (ns leo.main
   "Namespace that should be used by the published application only."
-  (:gen-class)
-  (:require
-   [leo]))
+  (:gen-class))
 
 (defn -main
   [& _args]
-  (leo/-main []))
+  ((requiring-resolve 'leo/-main)))
