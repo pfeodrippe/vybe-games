f03fcbb14fa928ce8d734d1d85a599dad817e2f4
Paulo Feodrippe <pfeodrippe@gmail.com>
2025-01-03 23:37:43 -0500

    Test Vybe v0.7.488

--- src/leo.clj
+++ src/leo.clj
@@ -14,10 +14,9 @@
    [vybe.util :as vy.u]
    [vybe.type :as vt]
    [vybe.network :as vn]
-   [clojure.edn :as edn]
-   [vybe.audio :as va]
+   #_[vybe.audio :as va]
    [vybe.math :as vm]
-   [overtone.core :refer :all])
+   #_[overtone.core :refer :all])
   (:import
    (org.vybe.flecs flecs)
    (org.vybe.raylib raylib)
@@ -29,8 +28,8 @@
 
 ;; Enable audio and require synth after it.
 #_(when-not *compile-files*
-  (va/audio-enable!)
-  (eval '(require '[overtone.inst.synth :as synth])))
+    (va/audio-enable!)
+    (eval '(require '[overtone.inst.synth :as synth])))
 
 #_(init)
 
@@ -89,14 +88,16 @@
     (my-noise [:tail early-g] :out_bus my-bus)
     (def sound-d (directional [:tail later-g] :in my-bus :out_bus 0))))
 
+(declare ddd my-music)
+
 (comment
 
   (def kk (sample "resources/audio/keyboard.mp3"))
   (def kk (sample "resources/audio/speech_1.mp3"))
 
   (definst my-music
-    [rate 0.4]
-    (* 7 (play-buf 2 kk (buf-rate-scale:ir kk) :loop 1 :rate rate)))
+    [rate 1.0]
+    (* 8 (play-buf 2 kk (buf-rate-scale:ir kk) :loop 1 :rate rate)))
 
   (my-music)
 
@@ -108,10 +109,10 @@
                                   bpf-snd (bpf input freq (/ freq 1000.0))
                                   snd (select (>= freq 100)
                                               [bpf-snd
-                                               #_(+ (* (/ (- 2400 freq) 2000.0)
-                                                       0.01
-                                                       (lf-noise0 2800))
-                                                    bpf-snd)
+                                               (+ (* (/ (- 2400 freq) 2000.0)
+                                                     0.1
+                                                     (lf-noise0 2800))
+                                                  bpf-snd)
                                                bpf-snd])]
                               (replace-out bus (+ snd bpf-snd)))))))
 
@@ -242,7 +243,7 @@
         amp (if (zero? d)
               1
               (/ 1 (* d d)))]
-    (va/sound
+    #_(va/sound
       (ctl sound-source :azim azim :elev elev :amp (* amp 10) :distance d))))
 
 #_ (init)
@@ -555,7 +556,7 @@
 
 #_(init)
 
-(defn get-data
+#_(defn get-data
   []
   (let [{:keys [arr timeline]} (last @va/*buffers)
         data (->> (mapv vector arr timeline)
@@ -679,20 +680,21 @@
                                                                                            [0.02 (+ 0.016 (wobble 0.01))
                                                                                             (+ 0.040 (wobble 0.01))]))}]]}
 
-        (va/sound (ctl ddd :freq (vr.c/remap (get-in (w (p :vg.gltf/Cube))
+        #_(va/sound (ctl ddd :freq (vr.c/remap (get-in (w (p :vg.gltf/Cube))
                                                      [vt/Translation
                                                       :y])
                                              -0.058608275 0.096363540
                                              2400 1000)))
 
-        (va/sound (ctl my-music :rate (vr.c/remap (get-in (w (p :vg.gltf/Cube))
+        #_(va/sound (ctl my-music :rate (vr.c/remap (get-in (w (p :vg.gltf/Cube))
                                                           [vt/Translation
                                                            :y])
                                                   -0.058608275 0.096363540
                                                   1.0 (/ (math/pow 2 11/12)
                                                          2))))
 
-        (vr.c/clear-background (vr/Color "#A98B39"))
+        (vr.c/clear-background (vr/Color "#000000")
+                               #_(vr/Color "#A98B39"))
         (vg/with-camera camera
           (draw-scene w))
 
