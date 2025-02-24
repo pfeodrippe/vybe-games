41a73bc189666963fb685a175efbc4f3fad44011
Paulo Feodrippe <pfeodrippe@gmail.com>
2025-02-23 22:01:15 -0500

    Use `vg.camera-move!`

--- src/minimal.clj
+++ src/minimal.clj
@@ -4,65 +4,14 @@
    [vybe.game :as vg]
    [vybe.raylib.c :as vr.c]
    [vybe.raylib :as vr]
-   [vybe.type :as vt]
-   [vybe.panama :as vp])
-  (:import
-   (org.vybe.raylib raylib)))
+   [vybe.type :as vt]))
 
 #_ (init)
-
-(def unit-z
-  (vt/Vector3 [0 0 -1]))
-
-(def unit-y
-  (vt/Vector3 [0 1 0]))
-
-(def unit-x
-  (vt/Vector3 [1 0 0]))
 
 (defn draw
   [w delta-time]
   ;; For debugging
   (def w w)
-
-  ;; TODO Move it into game.
-  (vf/with-query w [_ :vg/camera-active
-                    translation [:mut vt/Translation]
-                    rotation [:mut vt/Rotation]
-                    transform vt/Transform]
-    (let [key-down? #(vr.c/is-key-down %1)
-          move-forward (delay
-                         (fn [pos v]
-                           (vr.c/vector-3-add pos
-                                              (-> (vr.c/vector-3-transform unit-z transform)
-                                                  (vr.c/vector-3-subtract translation)
-                                                  (vr.c/vector-3-scale v)))))
-          move-right (delay
-                       (fn [pos v]
-                         (vr.c/vector-3-add pos
-                                            (-> (vr.c/vector-3-transform unit-z transform)
-                                                (vr.c/vector-3-subtract translation)
-                                                (vr.c/vector-3-scale v)
-                                                (vr.c/vector-3-cross-product unit-y)))))
-          new-translation (cond-> (vt/Translation [0 0 0])
-                            (key-down? (raylib/KEY_W)) (@move-forward 0.1)
-                            (key-down? (raylib/KEY_S)) (@move-forward -0.1)
-                            (key-down? (raylib/KEY_D)) (@move-right 0.1)
-                            (key-down? (raylib/KEY_A)) (@move-right -0.1))]
-      (when (or (realized? move-forward)
-                (realized? move-right))
-        (merge translation (-> (vr.c/vector-3-normalize new-translation)
-                               (vr.c/vector-3-scale (* delta-time 8.0))
-                               (vr.c/vector-3-add translation))))
-
-      (when (and (< 0 (vr.c/get-mouse-x) 600)
-                 (< 0 (vr.c/get-mouse-y) 600))
-        (merge rotation (-> rotation
-                            (vr.c/quaternion-multiply
-                             (vr.c/quaternion-from-axis-angle unit-y (* (:x (vr.c/get-mouse-delta))
-                                                                        -0.7
-                                                                        delta-time)))
-                            vr.c/quaternion-normalize)))))
 
   ;; Progress the systems (using Flecs).
   (vf/progress w delta-time)
@@ -70,7 +19,7 @@
   ;; Update physics (using Jolt).
   (vg/physics-update! w delta-time)
 
-  ;; Add some lights (from the blender model).
+  ;; Add some lights.
   (vg/draw-lights w)
   ;; You can also reset it to the default shader (no lights!) or use any othe
   ;; shader you want.
@@ -85,18 +34,6 @@
                       camera vt/Camera]
       (vg/with-camera camera
         (vg/draw-scene w)))
-
-    #_(vr.c/draw-text (-> (w (vf/path [:my/model :vg.gltf/Scene :vg.gltf/Cube :vg.gltf/lamp]))
-                          vybe.blender/entity-trs
-                          :rotation
-                          ((juxt :x :y :z :w))
-                          str)
-                      20 190 27 (vr/Color [228 128 228 255]))
-
-    #_(vybe.blender/entity-sync!
-       (w (vf/path [:my/model :vg.gltf/Scene :vg.gltf/Cube])))
-    #_(vybe.blender/entity-sync!
-       (w (vf/path [:my/model :vg.gltf/Scene :vg.gltf/Cube :vg.gltf/lamp])))
 
     (vr.c/draw-fps 510 570)))
 
@@ -113,7 +50,6 @@
                (fn [w]
                  (-> w
                      ;; Load model (as a resource).
-                     ;; You should have `minimal.glb` (a GLTF file) available.
                      (vg/model :my/model (vg/resource "com/pfeodrippe/vybe/model/minimal.glb")))))))
 
 #_(init)
--- src/noel.clj
+++ src/noel.clj
@@ -5,137 +5,103 @@
    [vybe.raylib.c :as vr.c]
    [vybe.raylib :as vr]
    [vybe.type :as vt]
-   [vybe.jolt :as vj]
-   [vybe.audio :as va]
-   #_[overtone.core :refer :all]
-   #_[overtone.inst.synth :as synth]))
+   [vybe.panama :as vp])
+  (:import
+   (org.vybe.raylib raylib)))
 
-#_(definst simple-flute [freq 880
-                       amp 0.5
-                       attack 0.4
-                       decay 0.5
-                       sustain 0.8
-                       release 1
-                       gate 1
-                       out 0]
-  (let [env  (env-gen (adsr attack decay sustain release) gate :action FREE)
-        mod1 (lin-lin:kr (sin-osc:kr 6) -1 1 (* freq 0.99) (* freq 1.01))
-        mod2 (lin-lin:kr (lf-noise2:kr 1) -1 1 0.2 1)
-        mod3 (lin-lin:kr (sin-osc:kr (ranged-rand 4 6)) -1 1 0.5 1)
-        sig (distort (* env (sin-osc [freq mod1])))
-        sig (* amp sig mod2 mod3)]
-    sig))
-
-#_(when-not *compile-files*
-  (va/audio-enable!)
-  #_(eval '(require '[overtone.inst.synth :as synth])))
-
-#_(va/sound
-  (inst-fx! synth/ks1 #_vybe.game.system/directional
-            fx-echo
-            :note (+ (rand-int 3) 50)
-            #_ #_:in (:bus synth/ks1))
-  (def aaa
-    (inst-fx! synth/ks1 vybe.game.system/directional
-              :azim (- (/ 3.14 2)) :elev 0 :distance 400.0
-              :in (:bus synth/ks1))))
-
-#_(demo
-    (clear-fx synth/ks1)
-    (keys (synth/ks1))
-    (let [v (synth/ks1 :note (+ (rand-int 3) 50)
-                       :azim (- (/ 3.14 3)) :elev 400.2 :distance 400.0)]
-      (ctl aaa :azim (- (/ 3.14 3)))))
-
-#_(vf/defobserver cccc w
-  [{:keys [contact-manifold body-1 body-2]} [:event vj/OnContactAdded]]
-  (let [l (* (vr.c/vector-3-length
-              (vr.c/vector-3-subtract (vj/linear-velocity body-1)
-                                      (vj/linear-velocity body-2)))
-             0.01)]
-
-    (va/sound
-     #_(merge w {(-> (get w (vybe.game.system/body-path body-1))
-                     (get vt/Eid)
-                     :id)
-                 [:vg/sound-source
-                  #_(-> (get w (vybe.game.system/body-path body-1))
-                        (get vt/Eid)
-                        :id)]})
-     (cond
-       (or (zero? (vj/motion-type body-1))
-           (zero? (vj/motion-type body-2)))
-       #_(synth/ks1 :note (+ (rand-int 3) 50)
-                    :amp l)
-       false
-
-       (= (vj/motion-type body-1) (vj/motion-type body-2))
-       #_(merge w {(vf/_)
-                   [:vg/sound-source
-                    [(vybe.panama/clone (get-in w [(-> (get w (vybe.game.system/body-path body-1))
-                                                       (get vt/Eid)
-                                                       :id)
-                                                   vt/Transform]))
-                     :global]]})
-       #_(do #_(def body-1 body-1)
-
-             (synth/ks1-demo :note (+ (rand-int 3) 90)
-                             :amp l
-                             #_(* (max (abs (:penetration_depth contact-manifold))
-                                       0.02)
-                                  20)))
-       false
-
-       :else
-       #_(synth/ks1 :note (+ (rand-int 3) 70)
-                    :amp l
-                    #_(* (max (abs (:penetration_depth contact-manifold))
-                              0.02)
-                         20))
-       false))))
-
-#_(stop)
+#_ (init)
 
 (defn draw
-  [w delta-time]
+  [{:keys [::vg/shader-shadowmap] :as w}
+   delta-time]
   ;; For debugging
   (def w w)
 
-  #_(cccc w)
+  #_(vybe.blender/entities-ignore! w [:pilot_a :pilot_b :pilot_c :pilot_d])
+  #_(vybe.blender/entity-sync! w)
+
+  ;; Accept inputs (mouse + WASD) to move the camera.
+  (vg/camera-move! w)
 
   ;; Progress the systems (using Flecs).
-  (vg/default-systems w)
   (vf/progress w delta-time)
 
   ;; Update physics (using Jolt).
   (vg/physics-update! w delta-time)
 
   ;; Add some lights (from the blender model).
+  #_(vr.c/rl-set-clip-planes -1000 1000)
   (vg/draw-lights w)
+  #_(vr.c/rl-set-clip-planes 0.01 1000)
+  ;; You can also reset it to the default shader (no lights!) or use any othe
+  ;; shader you want.
+  #_(vg/draw-lights w (get (::vg/shader-default w) vt/Shader))
 
-  ;; Render stuff into the screen (using Raylib).
-  (vg/with-drawing-fx w (vg/fx-painting w)
-    (vr.c/clear-background (vr/Color [255 20 100 255]))
+  (vg/draw-lights w (get shader-shadowmap vt/Shader) vg/draw-scene {:scene :vg.gltf.scene/track_scene})
+  (vg/with-target (w (vf/path [:my/model :vg.gltf/office :vg.gltf/tv :vg.gltf/screen]))
+    (vr.c/clear-background (vr/Color [50 100 100 255]))
+    (vg/with-camera (get (w (vf/path [:my/model :vg.gltf/track_camera])) vt/Camera)
+      (vg/draw-scene w {:scene :vg.gltf.scene/track_scene})))
 
-    ;; Here we do a query for the active camera (it's setup when loading the model).
-    (vf/with-query w [_ :vg/camera-active
-                      camera vt/Camera]
-      (vg/with-camera camera
-        (vg/draw-scene w)))
+  ;; Trigger animations continuously.
+  (vf/with-query w [_ [:or
+                       :vg.gltf.anim/pilot_axisAction.001
+                       :vg.gltf.anim/pilot_axisAction.002
+                       :vg.gltf.anim/pilot_axisAction.003
+                       :vg.gltf.anim/pilot_axisAction.004]
+                    _ :vg/animation
+                    e :vf/entity]
+    (conj e :vg.anim/active))
 
-    (vr.c/draw-fps 510 570)))
+  ;; Render stuff into the screen (using Raylib) using a built-in effect.
+  (vg/with-drawing
+    (vg/with-drawing-fx w (vg/fx-painting w {:dither-radius 0.2})
+      (vr.c/clear-background (vr/Color [20 20 20 255]))
+
+      ;; Here we do a query for the active camera (it's setup when loading the model).
+      (vf/with-query w [_ :vg/camera-active
+                        camera vt/Camera]
+        (vg/with-camera camera
+          (vg/draw-scene w {:scene :vg.gltf.scene/Scene})
+          #_(vg/draw-debug w)))
+
+      (vr.c/draw-fps 510 570))
+
+    (vg/with-drawing-fx w (vg/fx-painting w {:dither-radius 0.1})
+      (vr.c/draw-rectangle-pro (vr/Rectangle [320 325 220 120]) (vt/Vector2 [0 0]) 0
+                               (vr/Color [90 20 90 200]))
+      (vr.c/draw-rectangle-pro (vr/Rectangle [320 320 215 115]) (vt/Vector2 [0 0]) 0
+                               (vr/Color [205 234 235 255]))
+      (vr.c/gui-group-box (vr/Rectangle [330 330 200 100]) "Monster")
+      (vr.c/gui-dummy-rec (vr/Rectangle [340 340 180 80]) "Que tu quer???????????\n???")) ))
 
 #_ (init)
 
+(def screen-width 600)
+(def screen-height 600)
+
 (defn init
   []
-  (let [w (vf/make-world)]
-    ;; If you want to enable debugging (debug messages + clerk + flecs explorer),
-    ;; uncomment line below.
-    #_(vg/debug-init! w)
+  (let [w (vf/make-world)
+        model-path (vg/resource "noel.glb" {:throw-exception false
+                                            :target-folder "resources"})]
+    #_ (vg/debug-init! w)
+    #_(vybe.blender/gltf-model-path! model-path)
 
-    (vg/start! w 600 600 #'draw
+    (vg/start! w screen-width screen-height #'draw
                (fn [w]
+                 (vr.c/gui-load-style-sunny)
                  (-> w
-                     ;; Load model (as a resource).
-                     (vg/model :my/model (vg/resource "noel.glb")))))))
+                     (vg/model :my/model model-path))))))
+
+#_(init)
+
+(defn -main
+  [& _args]
+  ;; We start `init` in a future so it's out of the main thread,
+  ;; `vr/-main` will be in the main thread and it will loop the game draw
+  ;; function for us.
+  (future (init))
+
+  ;; Start main thread.
+  (vr/-main))
